6ba4f70c6ec71008cf71739044c9f07c277b6646
Taelliee <elitza_@mail.bg>
2025-11-01 02:21:00 +0200

    gui base classes and files

---
+++ VirtualVineyard/src/main/java/bg/tu_varna/sit/virtualvineyard/GUI/Launcher.java
@@ -0,0 +1,7 @@
+package bg.tu_varna.sit.virtualvineyard.GUI;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainApplication.launch(MainApplication.class, args);
+    }
+}
--- VirtualVineyard/src/main/java/bg/tu_varna/sit/virtualvineyard/GUI/MainController.java
+++ VirtualVineyard/src/main/java/bg/tu_varna/sit/virtualvineyard/GUI/MainController.java
@@ -3,7 +3,7 @@ package bg.tu_varna.sit.virtualvineyard.GUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
